be2984d27bf273dfa96f4d38f6ed70298c41a305
dlwlgus53 <dlwlgus4553@gmail.com>
2019-04-27 18:57:30 +0900

    peace

--- pa2/instagrapd.c
+++ pa2/instagrapd.c
@@ -11,8 +11,8 @@ child_proc(int conn)
 {
 	struct sockaddr_in serv_addr; 
 	int sock_fd ;
-	char buf[1024] ;
-	char * data = 0x0, * orig = 0x0 ;
+	char buffer[1024] ;
+	char * data = 0x0, * code = 0x0 ;
 	int len = 0 ;
 	int s ;
 
@@ -34,67 +34,37 @@ child_proc(int conn)
 		perror("connect failed : ") ;
 		exit(EXIT_FAILURE) ;
 	}
-	//send mode
-	data = "0";
-	while (len > 0 && (s = send(sock_fd, data, len, 0)) > 0) {
-			data += s ;
-			len -= s ;
-	}
-	//send code
-	while ( (s = recv(conn, buf, 1023, 0)) > 0 ) {
-		buf[s] = 0x0 ;
-		if (data == 0x0) {
-			data = strdup(buf) ;
-			len = s ;
-		}
-		else {
-			data = realloc(data, len + s + 1) ;
-			strncpy(data + len, buf, s) ;
-			data[len + s] = 0x0 ;
-			len += s ;
-		}
-
-		while (len > 0 && (s = send(sock_fd, data, len, 0)) > 0) {
-			data += s ;
-			len -= s ;
-		}
-	}
-
-	shutdown(sock_fd, SHUT_WR) ;
-	//send mode
-	data = "1";
-	while (len > 0 && (s = send(sock_fd, data, len, 0)) > 0) {
-			data += s ;
-			len -= s ;
-	}
-	//send input file
-	FILE *fp = fopen("1.in", "r");
-	data = 0x0 ;
-
-	if (fp == NULL){
-		printf("Error opening file\n");
-		exit(1);
-	}
-
-	while( fgets(buf, 1024, fp) != NULL) {
-		data = buf ;
-		len = strlen(buf) ;
-		s = 0 ;
-		while (len > 0 && (s = send(sock_fd, data, len, 0)) > 0) {
-			data += s ;
-			len -= s ;
-		}
-		
-	}	
-	fclose(fp);
-	shutdown(sock_fd, SHUT_WR) ;
-	//input file send finish
-
-	//output wating
-	char buffer[1024] ;
-	data = 0x0 ;
-	len = 0 ;
-	while ( (s = recv(sock_fd, buffer, 1023, 0)) > 0 ) {
+//    //send mode
+//    printf("send mode 0 start\n");
+//    data = "0";
+//    len = 2;
+//    while (len > 0 && (s = send(sock_fd, data, len, 0)) > 0) {
+//            data += s ;
+//            len -= s ;
+//    }
+//    shutdown(sock_fd, SHUT_WR) ;
+//    data = 0x0;
+//    while ( (s = recv(sock_fd, buffer, 1023, 0)) > 0 ) {
+//        buffer[s] = 0x0 ;
+//        if (data == 0x0) {
+//            data = strdup(buffer) ;
+//            len = s ;
+//        }
+//        else {
+//            data = realloc(data, len + s + 1) ;
+//            strncpy(data + len, buffer, s) ;
+//            data[len + s] = 0x0 ;
+//            len += s ;
+//        }
+//
+//    }
+//    printf("send mode 0 end\n");
+//    //send mode end
+//
+    //recv code
+    data = 0x0; len=0;
+    printf("recev code start\n");
+	while ( (s = recv(conn, buffer, 1023, 0)) > 0 ) {//receive code from submitter
 		buffer[s] = 0x0 ;
 		if (data == 0x0) {
 			data = strdup(buffer) ;
@@ -106,23 +76,217 @@ child_proc(int conn)
 			data[len + s] = 0x0 ;
 			len += s ;
 		}
-
 	}
-	//output wating end
-	printf(">%s\n", data) ;
-	
-	orig = data ;
-	//start send to submitter
-	while (len > 0 && (s = send(conn, data, len, 0)) > 0) {
-		data += s ;
-		len -= s ;
-	}
-	shutdown(conn, SHUT_WR) ;
-	//end send to submitter
+    code=data;
+    len = strlen(data) ;
+    printf("%s" ,data);
+    while (len > 0 && (s = send(conn, data, len, 0)) > 0) {
+        data += s ;
+        len -= s ;
+    }
+    shutdown(conn, SHUT_WR) ;
+    printf("recev code end\n");
+    //recv code end
+    
+    //make mode bit in code
+    strcat(code, "0");
+    len++;
+    
+    //send code
+    printf("send code start\n");
+    printf("%s" ,code);
+    len = strlen(code) ;
+    while (len > 0 && (s = send(sock_fd, code, len, 0)) > 0) {//send code to worker
+        code += s ;
+        len -= s ;
+    }
+	shutdown(sock_fd, SHUT_WR) ;
+    data = 0x0; len=0;
+    while ( (s = recv(sock_fd, buffer, 1023, 0)) > 0 ) {
+        buffer[s] = 0x0 ;
+        if (data == 0x0) {
+            data = strdup(buffer) ;
+            len = s ;
+        }
+        else {
+            data = realloc(data, len + s + 1) ;
+            strncpy(data + len, buffer, s) ;
+            data[len + s] = 0x0 ;
+            len += s ;
+        }
+        
+    }
+    printf("send code end\n");
+    //send code end
+    
+//    //send mode for input
+//    printf("send mode 1 start\n");
+//    data = "1";
+//     len = 2;
+//    while (len > 0 && (s = send(sock_fd, data, len, 0)) > 0) {
+//            data += s ;
+//            len -= s ;
+//    }
+//    shutdown(sock_fd, SHUT_WR) ;
+//    data = 0x0;
+//    while ( (s = recv(conn, buffer, 1023, 0)) > 0 ) {//receive code from submitter
+//        buffer[s] = 0x0 ;
+//        if (data == 0x0) {
+//            data = strdup(buffer) ;
+//            len = s ;
+//        }
+//    }
+//    printf("send mode 1 end\n");
+//    //send mode end
+//
+    //file open
+    
+//    FILE *fp = fopen("1.in", "r");
+//    data = 0x0 ;
+//
+//    if (fp == NULL){
+//        printf("Error opening file\n");
+//        exit(1);
+//    }
+//
+//    //send input and wait output
+//     printf("send input and wait output start\n");
+//    while( fgets(buffer, 1024, fp) != NULL) {
+//        data = buffer ;
+//        len = strlen(buffer) ;
+//        s = 0 ;
+//    }
+//    //input value with code
+//    strcat(data, "1");
+//    len++;
+//    while (len > 0 && (s = send(sock_fd, data, len, 0)) > 0) {
+//        data += s ;
+//        len -= s ;
+//    }
+//    
+//    shutdown(sock_fd, SHUT_WR) ;
+//    fclose(fp);
+//    data = 0x0; len=0;
+//    while ( (s = recv(sock_fd, buffer, 1023, 0)) > 0 ) {
+//        buffer[s] = 0x0 ;
+//        if (data == 0x0) {
+//            data = strdup(buffer) ;
+//            len = s ;
+//        }
+//        else {
+//            data = realloc(data, len + s + 1) ;
+//            strncpy(data + len, buffer, s) ;
+//            data[len + s] = 0x0 ;
+//            len += s ;
+//        }
+//        
+//    }
+//    printf("send input and wait output end\n");
+//    //send input and get output end
+//    
+//    //output
+//    printf(">%s\n", data) ;
+//
+//    //start send to submitter
+//    printf("send output start\n");
+//    while (len > 0 && (s = send(conn, data, len, 0)) > 0) {
+//        data += s ;
+//        len -= s ;
+//    }
+//    shutdown(conn, SHUT_WR) ;
+//    printf("send output end\n");
+//    //end send to submitter
 
-	
-	if (orig != 0x0) 
-		free(orig) ;
+    free(data);
+}
+
+void
+child_proc2(int conn)
+{
+    struct sockaddr_in serv_addr;
+    int sock_fd ;
+    char buffer[1024] ;
+    char * data = 0x0, * code = 0x0 ;
+    int len = 0 ;
+    int s ;
+    
+    sock_fd = socket(AF_INET, SOCK_STREAM, 0) ;
+    if (sock_fd <= 0) {
+        perror("socket failed : ") ;
+        exit(EXIT_FAILURE) ;
+    }
+    
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(8564);
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        perror("inet_pton failed : ") ;
+        exit(EXIT_FAILURE) ;
+    }
+    
+    if (connect(sock_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        perror("connect failed : ") ;
+        exit(EXIT_FAILURE) ;
+    }
+    //input file open
+    FILE *fp = fopen("1.in", "r");
+    
+    if (fp == NULL){
+        printf("Error opening file\n");
+        exit(1);
+    }
+    
+    //send input and wait output
+    printf("send input and wait output start\n");
+    while( fgets(buffer, 1024, fp) != NULL) {
+        data = buffer ;
+        len = strlen(buffer) ;
+        s = 0 ;
+    }
+    
+    //input value with code
+    strcat(data, "1");
+    printf("input :\n %s", data);
+    len++;
+    while (len > 0 && (s = send(sock_fd, data, len, 0)) > 0) {
+        data += s ;
+        len -= s ;
+    }
+    
+    shutdown(sock_fd, SHUT_WR) ;
+    fclose(fp);
+    data = 0x0; len=0;
+    while ( (s = recv(sock_fd, buffer, 1023, 0)) > 0 ) {
+        buffer[s] = 0x0 ;
+        if (data == 0x0) {
+            data = strdup(buffer) ;
+            len = s ;
+        }
+        else {
+            data = realloc(data, len + s + 1) ;
+            strncpy(data + len, buffer, s) ;
+            data[len + s] = 0x0 ;
+            len += s ;
+        }
+        
+    }
+    printf("send input and wait output end\n");
+    //send input and get output end
+    
+    //output
+    printf(">%s\n", data) ;
+    
+    //start send to submitter
+    printf("send output start\n");
+    while (len > 0 && (s = send(conn, data, len, 0)) > 0) {
+        data += s ;
+        len -= s ;
+    }
+    shutdown(conn, SHUT_WR) ;
+    printf("send output end\n");
+    //end send to submitter
+    
+    free(data);
 }
 
 int 
@@ -164,6 +328,7 @@ main(int argc, char const *argv[])
 
 		if (fork() > 0) {
 			child_proc(new_socket) ;
+            child_proc2(new_socket) ;
 		}
 		else {
 			close(new_socket) ;
